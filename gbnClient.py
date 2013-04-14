c28c0a589906d1b782b330095792cd73ba9e7976
Shankar <shankar.c.k.02@gmail.com>
2013-04-13 23:11:44 -0400

    v2.0

--- gbnClient.py
+++ gbnClient.py
@@ -1,85 +1,103 @@
 import sys
 import socket
 import time
+import struct
 import threading
 
-lastSent = 0
-acked = 0
+lastSent = 0			#LAST SENT SEQUENCE NUMBER
+acked = 0				#SEQUENCE NUMBER OF LAST PACKET ACKED
+expectedAck = 0			#SEQUENCE NUMBER NEXT EXPECTED ACK
 
+
+#Thread that reads the file continuously
 class fileReader(threading.Thread):
 	def __init__(self, cmdInput, cSock, receiver):
 		threading.Thread.__init__(self)
-		self.host = cmdInput[0]
-		self.port = int(cmdInput[1])
-		self.file = cmdInput[2]
-		self.n    = int(cmdInput[3])
-		self.MSS  = int(cmdInput[4])
+		self.host = cmdInput[0]				#SERVER IP ADDRESS
+		self.port = int(cmdInput[1])		#SERVER PORT
+		self.file = cmdInput[2]				#FILE TO TRANSMIT
+		self.n    = int(cmdInput[3])		#WINDOW SIZE
+		self.MSS  = int(cmdInput[4])		#MAXIMUM SEGMENT SIZE
 		self.sock = cSock
 		self.r = receiver
-		self.start()
-		#self.seqNum = 0
-		
+		self.start()		
 		
 	def run(self):
-		self.rdt_send()
-	
-	def computeChecksum(self, data):
-		pass
-	
-	def formPacket(self, data, seq):
-		#32 bit sequence number
-		#16 bit check of the data part
-		#16 bit 0101010101010101 -- Indicates data packet(in int 21845)
-		seqNum = '{0:032b}'.format(seq)
-		checksum = self.computeChecksum(data)
-		dataIndicator = '{0:016b}'.format(21845)
-		packet = seqNum+checksum+dataIndicator+data
-		
+		self.rdt_send()	
 		
 	def rdt_send(self):
 		fileHandle = open(self.file,'rb')
-		i = 0
+		currSeq = 0
 		global lastSent
 		global acked
 		sendMsg = ''
 		b = True
 		while b:
 			b = fileHandle.read(1)
-			sendMsg += str(b, 'UTF-8')
-			if len(sendMsg) == self.MSS:
-				#packet = formPacket(c, self.seqNum)
-				#packet = bytes(sendMsg,'UTF-8')
-				#self.seqNum += 1		
-				while lastSent - acked >= self.n:
+			sendMsg += str(b,'UTF-8')
+			if len(sendMsg) == self.MSS or (not b):		
+				while currSeq-acked >= self.n:
 					pass
-				sender(sendMsg,i)
-				i += 1
-				#self.sock.sendto(packet,(self.host, self.port))
+				sender(self.sock, self.host, self.port, sendMsg, currSeq)    #Thread spawned to handle a single packet
+				currSeq += 1
 				sendMsg = ''
-		self.sock.sendto(bytes('end','UTF-8'),(self.host, self.port))	
-		lastSent = i
+						
+		sendMsg = '00000end11111'
+		sender(self.sock, self.host, self.port, sendMsg,currSeq)		#Thread spawned to send the end packet
+		lastSent = currSeq
 		fileHandle.close()
 
+		
+#Thread Class to handle the sending of a single packet
 class sender(threading.Thread):
-	def __init__(self, msg, s):
+	def __init__(self, cSock, hst, prt, msg, s):
 		threading.Thread.__init__(self)
 		self.timer = time.time()
-		self.data = msg
-		self.seqNum = s
+		self.data = msg				#DATA OF 1 MSS SIZE TO BE SENT
+		self.seqNum = s				#SEQUENCE NUMBER OF THE PACKET
+		self.sock = cSock
+		self.host = hst				#SERVER IP ADDRESS
+		self.port = prt				#SERVER PORT
 		self.start()
-		
+	
+	def computeChecksum(self, data):
+		sum = 0
+		for i in range(0, len(data), 2):
+			if i+1 < len(data):
+				data16 = ord(data[i]) + (ord(data[i+1]) << 8)		#To take 16 bits at a time
+				interSum = sum + data16
+				sum = (interSum & 0xffff) + (interSum >> 16)		#'&' to ensure 16 bits are returned
+		return ~sum & 0xffff										#'&' to ensure 16 bits are returned
+				
+	def formPacket(self, data, seq):
+		#32 bit sequence number
+		#16 bit check of the data part
+		#16 bit 0101010101010101 -- Indicates data packet(in int 21845)
+		seqNum = struct.pack('=I',seq)
+		checksum = struct.pack('=H',self.computeChecksum(data))		#Computes the checksum of data
+		dataIndicator = struct.pack('=H',21845)
+		packet = seqNum+checksum+dataIndicator+bytes(data,'UTF-8')
+		return packet
+	
 	def run(self):
 		global acked
-		packet = bytes(sendMsg,'UTF-8')
+		packet = self.formPacket(self.data, self.seqNum)				#Packets are created here
 		self.sock.sendto(packet,(self.host, self.port))
-		while acked < self.seqNum:
-			diff = time.time() - self.timer
-			if  diff < 5:
-				pass
-			else:
-				if acked < self.seqNum:
-					self.sock.sendto(packet,(self.host, self.port))
-				
+		try:
+			while acked < self.seqNum:
+				diff = time.time() - self.timer
+				if  diff < 5:									#RETRANSMISSION time = 5 seconds
+					pass
+				else:
+					if acked < self.seqNum:								#Rechecking the ACK
+						print('RETRANSMITTING PACKET - '+str(self.seqNum))
+						self.sock.sendto(packet,(self.host, self.port))	#RETRANSMISSION of time-out packets(No ACK Received)
+		except:
+			print('Server closed its connection')
+			self.sock.close()
+		
+		
+#Thread Class to receive the ACK Packets from the Server
 class receiver(threading.Thread):
 	def __init__(self, cmdInput, cSock):		
 		threading.Thread.__init__(self)
@@ -89,33 +107,48 @@ class receiver(threading.Thread):
 		self.n    = int(cmdInput[3])
 		self.MSS  = int(cmdInput[4])
 		self.sockAddr = cSock
-		#self.senderRef = sender
 		self.start()
-		
+	
+	def parseMsg(self, msg):
+		sequenceNum = struct.unpack('=I', msg[0:4])			#Sequence Number Acked by the server
+		zero16 = struct.unpack('=H', msg[4:6])				#16 bit field with all 0's
+		identifier = struct.unpack('=H', msg[6:])			#16 bit field to identify the ACK packets
+		return sequenceNum, zero16, identifier
 		
 	def run(self):
 		print('Receiver Spawned')
 		global lastSent
 		global acked		
-		while lastSent <= 0 or  acked < lastSent:			
-			ackReceived, server_addr = self.sockAddr.recvfrom(2048)	
-			acked += 1
-			print(ackReceived.decode('UTF-8'))
-		
+		global expectedAck
+		try:
+			while lastSent <= 0 or  acked < lastSent:			
+				ackReceived, server_addr = self.sockAddr.recvfrom(2048)			#Receives the ACK packets 
+				sequenceNum , zero16, identifier = self.parseMsg(ackReceived)
+				
+				#16 bit identifier field to identify the ACK packets - 1010101010101010 [in int 43690]		
+				if int(identifier[0]) == 43690 and expectedAck == int(sequenceNum[0]):
+					acked = int(sequenceNum[0])
+					expectedAck = acked+1
+				print('Packet of Seq No.'+str(sequenceNum[0])+' Acked')
+				
+		except:
+			print('Server closed its connection')
+			self.sockAddr.close()
+
+			
 def main():
 	host = sys.argv[1]
 	port = int(sys.argv[2])
 	cliSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)	
 	cliSocket.bind(('',3334)) 
-	ackReceiver = receiver(sys.argv[1:], cliSocket)
-	fileHandler = fileReader(sys.argv[1:],cliSocket, ackReceiver)
-	ackReceiver.join()
-	dataSender.join()
-	cliSocket.close()
 	
-
-	
-	
+	ackReceiver = receiver(sys.argv[1:], cliSocket)					#Thread that receives ACKs from the Server
+	fileHandler = fileReader(sys.argv[1:],cliSocket, ackReceiver) 	#Thread that reads the file and sending of packets
+	fileHandler.join() 			#Main thread waits till the sender finishes
+	ackReceiver.join()			#Main thread waits till the ACK receiver finishes
+	if cliSocket:
+		cliSocket.close()
+		
 	
 if __name__ == '__main__':	
 	main()	
--- gbnServer.py
+++ gbnServer.py
@@ -1,32 +1,80 @@
 import sys
 import socket
+import struct
+import random
 import threading
 
-#### ack indications in int 43690
 
+def parseMsg(msg):					#Parsing the Message received from client
+	header = msg[0:8]
+	data = msg[8:]	
+	sequenceNum = struct.unpack('=I',header[0:4])		
+	checksum = struct.unpack('=H',header[4:6])
+	identifier = struct.unpack('=H',header[6:])
+	dataDecoded = data.decode('UTF-8')	
+	return sequenceNum, checksum, identifier, dataDecoded
+	
+def formAckPackets(seqAcked):
+	seqNum 		 = struct.pack('=I', seqAcked)	#SEQUENCE NUMBER BEING ACKED	
+	zero16 	 	 = struct.pack('=H', 0)
+	ackIndicator = struct.pack('=H',43690)		#ACK INDICATOR - 1010101010101010[INT 43690]
+	ackPacket = seqNum+zero16+ackIndicator
+	return ackPacket
+
+def verifyChecksum(data, checksum):
+	sum = 0
+	
+	for i in range(0, len(data), 2):
+		if i+1 < len(data):
+			data16 = ord(data[i]) + (ord(data[i+1]) << 8)		#To take 16 bits at a time
+			interSum = sum + data16
+			sum = (interSum & 0xffff) + (interSum >> 16)		#To ensure 16 bits
+	currChk = sum & 0xffff 
+	result = currChk & checksum
+	
+	if result == 0:
+		return True
+	else:
+		return False
+	
 def main():
 	#port filename probability
-	port = int(sys.argv[1])
-	filename = sys.argv[2]
-	prob = sys.argv[3]
+	
+	port = int(sys.argv[1])		#PORT ON WHICH SERVER WILL ACCEPT UDP PACKETS
+	filename = sys.argv[2]		#NAME OF THE NEW FILE CREATED
+	prob = float(sys.argv[3])	#PACKET DROP PROBABILITY
+	expSeqNum = 0				
 	flag = True
+	
 	soc  = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)	
 	host = socket.gethostname()
 	soc.bind((host,port)) 
-	fileHandler = open(filename,'w')
-	while flag:	
-		receivedMsg, sender_addr = soc.recvfrom(25)	
-		msg = receivedMsg.decode('UTF-8')
-		fileHandler.write(msg)
-		if msg != 'end':
-			print('Sending Acknowledge')
-			ackPacket = bytes('packet acknowledged','UTF-8')		
-			soc.sendto(ackPacket,sender_addr)
-		else:
-			flag = False
-	fileHandler.close()
-	#soc.close()	
 	
-
+	fileHandler = open(filename,'w')
+	
+	while flag:	
+		receivedMsg, sender_addr = soc.recvfrom(1024)			#Receive packets sent by client
+		
+		if random.uniform(0,10) > prob:							#PACKET MAY BE DROPPED BASED ON RANDOM VALUE
+			sequenceNum, checksum, identifier, data = parseMsg(receivedMsg) 
+		
+			if expSeqNum == int(sequenceNum[0]):				#If the expected Packet
+				chksumVerification = verifyChecksum(data, int(checksum[0]))
+				if chksumVerification == True:
+					if data != '00000end11111':					#If not the END Packet
+						fileHandler.write(data)					#Write to FILE
+						ackPacket = formAckPackets(int(sequenceNum[0]))		#Generating ACK Packet
+						soc.sendto(ackPacket,sender_addr)					#Sending ACK
+					else:
+						flag = False
+					expSeqNum += 1
+		else:
+			print('PACKET DROPPPED')	#Packet dropped if randomValue <= probability
+				
+			
+	fileHandler.close()
+	print('File Received Successfully at the Server')
+	soc.close()	
+	
 if __name__ == '__main__':	
 	main()
